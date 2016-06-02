9f08e606157103e68a24e5ee98a42e21d93afefa
zcrome <soportezcrome@gmail.com>
2016-06-02 15:50:16 -0500

    Datos Laborales
    Botones de seleccion entre constante e intermitente.
    Captura de inputs para Remuneracion Constante

--- js/angular-app/controllers/main.controller.js
+++ js/angular-app/controllers/main.controller.js
@@ -6,13 +6,28 @@
         'MainController',
         ['$scope',
          function($scope){
-          //VARIABLES!!
+          /*******local variables!!*******/
           var self = $scope;
 
-          console.log("HALO WORLD");
+					/*******web variables*******/
 
+					//REMUNERATION VARIABLES
+					//type 1 Remuneracion constante
+					//type 2 remuneracion intermitente
+					self.dateRetirement = null;
+					self.typeRemuneration = null;
+					self.mothEarnIt = 0;
+					self.cantPaymentPerYear = null;
 
-
-
+					// Semantic UI Range
+					$('#range-3').range({
+							min: 20,
+							max: 75,
+							start: 65,
+							onChange: function(value) {
+									$('#display-3').html(value);
+									self.dateRetirement = value;
+							}
+					});
 			}]);
 })(window.angular);
