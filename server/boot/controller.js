ca6043681267af3ce70257789613c2b383128ee3
zcrome <soportezcrome@gmail.com>
2016-06-28 12:11:04 -0500

    Permisos para el modelo Usuario

---
+++ server/boot/controller.js
@@ -0,0 +1,18 @@
+module.exports = function(server) {
+
+  var router = server.loopback.Router();
+  var Usuario = server.models.Usuario;
+
+  server.models.Usuario.settings.acls = require('./user-acls.json');
+
+
+
+
+
+
+
+
+
+  server.use(router);
+
+};
